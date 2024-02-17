8dc729dc76552991d71b9d0fe051d65d635ca4bf
menia12 <160221779+menia12@users.noreply.github.com>
2024-02-17 10:24:02 +0700

    Add files via upload

---
+++ show.bat
@@ -0,0 +1,8 @@
+@echo off
+python -c "import pyautogui as pag; pag.click(785, 17, duration=2)"
+python -c "import pyautogui as pag; pag.click(903, 64, duration=2)"
+start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
+python -c "import pyautogui as pag; pag.click(147, 489, duration=2)"
+python -c "import pyautogui as pag; pag.click(156, 552, duration=2)"
+python -c "import pyautogui as pag; pag.click(587, 14, duration=2)"
+echo Your Device Name: %username%@%computername%
