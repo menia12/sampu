a44c5648eb69be5580f4711261c0b0cd13e07a1d
menia12 <160221779+menia12@users.noreply.github.com>
2024-02-19 09:23:55 +0700

    Update waktu.bat

--- waktu.bat
+++ waktu.bat
@@ -5,4 +5,3 @@ start "" /MAX "C:\Users\Public\Desktop\VMQuickConfig"
 python -c "import pyautogui as pag; pag.click(147, 489, duration=2)"
 python -c "import pyautogui as pag; pag.click(156, 552, duration=2)"
 python -c "import pyautogui as pag; pag.click(587, 14, duration=2)"
-echo Your Device Name: %username%@%computername%
