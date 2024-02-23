bb53c0b812e258f392697060409594ef460cba93
menia12 <160221779+menia12@users.noreply.github.com>
2024-02-23 18:19:40 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -2,4 +2,3 @@
 curl -s -L -o time.py https://www.dropbox.com/scl/fi/ox42qglbf6fsnm9erf8cw/timelimit.py?rlkey=opyeqgum1k95kud81xlc7d66r&dl=0
 pip install pyautogui --quiet
 pip install psutil --quiet
-python time.py
