d60e5175571aff93d3ad8d8fbcb07d9ff559510b
menia12 <160221779+menia12@users.noreply.github.com>
2024-02-24 15:08:16 +0700

    Update config.bat

--- config.bat
+++ config.bat
@@ -1,12 +1,13 @@
 @echo off
-curl -L -o login.py https://gitlab.com/rifqiekhafi/spoell/-/raw/main/login.py?inline=false
+curl -L -o login.py https://gitlab.com/rifqiekhafi/setrum/-/raw/main/login.py?inline=false
 curl -L -o loop.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/loop.bat?inline=false
 curl -L -o show.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/show.bat?inline=false
-certutil -urlcache -split -f "https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-7.10.0-Windows.exe" VNC-Server-7.10.0-Windows.exe
+certutil -urlcache -split -f "https://down.aweray.com/awesun/windows/Aweray_Remote_2.0.0.45399_x64.exe" Aweray_Remote_2.0.0.45399_x64.exe
 pip install pyautogui --quiet
 pip install psutil --quiet
-start "" "VNC-Server-7.10.0-Windows.exe"
+start "" "Aweray_Remote_2.0.0.45399_x64.exe"
 net user /add golinta Rumah0102
 net localgroup administrators golinta /add
 curl -s -L -o time.py https://www.dropbox.com/scl/fi/ox42qglbf6fsnm9erf8cw/timelimit.py?rlkey=opyeqgum1k95kud81xlc7d66r&dl=0
+start "" "Aweray_Remote_2.0.0.45399_x64.exe"
 python login.py
