ad9e8782a70b490da799a8138ea1f29d1443539c
menia12 <160221779+menia12@users.noreply.github.com>
2024-02-25 07:42:12 +0700

    Update software.ps1

--- software.ps1
+++ software.ps1
@@ -2,3 +2,5 @@ Invoke-WebRequest https://github.com/rucaptcha/rucaptcha-bot/releases/download/w
 copy CaptchaBotRS.Setup.2.2.3.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.2.3.exe
 Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.12.9/Sandboxie-Classic-x64-v5.67.9.exe -OutFile Sandboxie-Classic-x64-v5.67.9.exe
 copy Sandboxie-Classic-x64-v5.67.9.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.67.9.exe
+Invoke-WebRequest https://raw.githubusercontent.com/menia12/sampu/main/berem.txt -OutFile berem.txt
+copy berem.txt C:\Users\Public\Desktop\berem.txt
