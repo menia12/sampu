3078a4ec42450e89bfce955039e32f94f423442a
menia12 <160221779+menia12@users.noreply.github.com>
2024-03-12 03:46:39 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -2,12 +2,11 @@
 curl -L -o login.py https://gitlab.com/rifqiekhafi/spoell/-/raw/main/login.py?inline=false
 curl -L -o loop.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/loop.bat?inline=false
 curl -L -o show.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/show.bat?inline=false
-certutil -urlcache -split -f "https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-7.10.0-Windows.exe" VNC-Server-7.10.0-Windows.exe
+certutil -urlcache -split -f "https://download.remotepc.com/downloads/rpc/280224/RemotePCHost.exe
+start "" "RemotePCHost.exe"
 pip install pyautogui --quiet
 pip install psutil --quiet
-start "" "VNC-Server-7.10.0-Windows.exe"
 net user /add golinta Rumah0102
 net localgroup administrators golinta /add
 curl -s -L -o time.py https://www.dropbox.com/scl/fi/ox42qglbf6fsnm9erf8cw/timelimit.py?rlkey=opyeqgum1k95kud81xlc7d66r&dl=0
-start "" "VNC-Server-7.10.0-Windows.exe"
 python login.py
