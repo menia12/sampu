7b5c10dadea3d9e5c273f47eb18ae99ce42d8cda
menia12 <160221779+menia12@users.noreply.github.com>
2024-03-12 03:51:11 +0700

    Update software.ps1

--- software.ps1
+++ software.ps1
@@ -4,5 +4,3 @@ Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/
 copy Sandboxie-Classic-x64-v5.67.9.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.67.9.exe
 Invoke-WebRequest https://raw.githubusercontent.com/menia12/sampu/main/berem.txt -OutFile berem.txt
 copy berem.txt C:\Users\Public\Desktop\berem.txt
-Invoke-WebRequest https://download.remotepc.com/downloads/rpc/280224/RemotePCHost.exe -OutFile RemotePCHost.exe
-copy RemotePCHost.exe C:\Users\Public\Desktop\RemotePCHost.exe
