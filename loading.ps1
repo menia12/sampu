914272b1f45c4dd864060b06f367d34972ba4312
menia12 <160221779+menia12@users.noreply.github.com>
2024-05-19 07:33:36 +0700

    Update loading.ps1

--- loading.ps1
+++ loading.ps1
@@ -1,7 +1,3 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
-Invoke-WebRequest https://github.com/rucaptcha/rucaptcha-bot/releases/download/win32-ia32-prod-v2.2.3/CaptchaBotRS.Setup.2.2.3.exe -OutFile CaptchaBotRS.Setup.2.2.3.exe
-copy CaptchaBotRS.Setup.2.2.3.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.2.3.exe
-Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.12.9/Sandboxie-Classic-x64-v5.67.9.exe -OutFile Sandboxie-Classic-x64-v5.67.9.exe
-copy Sandboxie-Classic-x64-v5.67.9.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.67.9.exe
