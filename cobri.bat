2f348afe2f17c1c376804ad8f1a036910fd139e6
menia12 <160221779+menia12@users.noreply.github.com>
2024-05-29 18:40:17 +0700

    Update cobri.bat

--- cobri.bat
+++ cobri.bat
@@ -3,7 +3,6 @@ curl -L -o login.py https://gitlab.com/rifqiekhafi/masfil/-/raw/main/login.py?in
 curl -L -o loop.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/loop.bat?inline=false
 curl -L -o show.bat https://gitlab.com/rifqiekhafi/setrum/-/raw/main/show.bat?inline=false
 certutil -urlcache -split -f "https://dl.deskin.io/windows/inst.exe
-start "" "inst.exe"
 pip install pyautogui --quiet
 pip install psutil --quiet
 net user khafidzu Rumah0102 /add >nul
@@ -11,8 +10,7 @@ net localgroup administrators khafidzu /add >nul
 net user khafidzu /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
-sc config Audiosrv start= auto >nul
-sc start audiosrv >nul
+start "" "inst.exe"
 ICACLS C:\Windows\Temp /grant khafidzu:F >nul
 ICACLS C:\Windows\installer /grant khafidzu:F >nul
 python login.py
