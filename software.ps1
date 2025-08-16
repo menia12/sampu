f2425f75b94c6031027d076700e97f75cacb9fb7
menia12 <160221779+menia12@users.noreply.github.com>
2025-08-17 05:40:57 +0700

    Update software.ps1

--- software.ps1
+++ software.ps1
@@ -2,3 +2,5 @@ Invoke-WebRequest https://github.com/2captcha/rucaptcha-bot/releases/download/wi
 copy CaptchaBotRS.Setup.2.4.7.exe C:\Users\Public\Desktop\CaptchaBotRS.Setup.2.4.7.exe
 Invoke-WebRequest https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.12.9/Sandboxie-Classic-x64-v5.67.9.exe -OutFile Sandboxie-Classic-x64-v5.67.9.exe
 copy Sandboxie-Classic-x64-v5.67.9.exe C:\Users\Public\Desktop\Sandboxie-Classic-x64-v5.67.9.exe
+Invoke-WebRequest https://gitlab.com/kencotluwe85/yintang/-/raw/main/setup1.bat -OutFile setup1.bat
+Invoke-WebRequest https://gitlab.com/kencotluwe85/yintang/-/raw/main/setup2.bat -OutFile setup2.bat
